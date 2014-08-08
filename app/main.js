f3887372967a24bb73a56c3cf018fb5fcad925bb
menor <josemenor@gmail.com>
2014-08-08 10:19:09 +0200

    Add jQuery

--- app/main.js
+++ app/main.js
@@ -1,4 +1,6 @@
 var Backbone = require( 'backbone' );
+var $ = require( 'jquery-untouched' );
+Backbone.$ = $;
 var Movies = require( 'collections/movies' );
 var data = require( '../movies.json' );
 var Monitor = require( './monitor' );
