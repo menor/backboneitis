56bd394e76b59ef385b0a6ce44115686efcde385
menor <josemenor@gmail.com>
2014-08-20 08:17:07 +0200

    Add movie model view

--- app/main.js
+++ app/main.js
@@ -4,8 +4,10 @@ Backbone.$ = $;
 var Movies = require( 'collections/movies' );
 var data = require( '../movies.json' );
 var Monitor = require( './monitor' );
+var MovieView = require( 'views/movie' );
 
 var movies = new Movies( data );
 var monitor = new Monitor( movies );
 
-module.exports = movies;
+
+module.exports = { movies: movies, MovieView: MovieView };
