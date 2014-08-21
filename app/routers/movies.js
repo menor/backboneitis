8eb4a933fd35202cd3f58eb486fc8669784bef4d
menor <josemenor@gmail.com>
2014-08-21 12:05:06 +0200

    Add movies routing.

---
+++ app/routers/movies.js
@@ -0,0 +1,36 @@
+var Backbone = require( 'backbone' );
+
+// Data
+var Movies = require( 'collections/movies' );
+var data = require( '../../movies.json' );
+var movies = new Movies( data );
+
+// Views
+var MoviesList = require( 'views/moviesList' );
+
+var MoviesRouter = Backbone.Router.extend( {
+
+  routes: {
+    'movies/:id': 'selectMovie',
+    '':           'showMain'
+  },
+
+  selectMovie: function( id ) {
+    this.moviesList.render();
+    this.movies.selectByID( id );
+  },
+
+  showMain: function() {
+    this.moviesList.render();
+  },
+
+  initialize: function() {
+    this.movies = movies;
+    this.moviesList = new MoviesList( {
+      el: options.el,
+      collection: movies
+    } );
+  }
+} );
+
+module.exports = MoviesRouter;
